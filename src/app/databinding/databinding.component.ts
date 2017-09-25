df7e5e99c5763c57bc5bf449b165696098478089
aniruddha <aniruddha.kudalkar@gmail.com>
2017-09-25 19:35:56 +0530

    databinding : property binding

--- src/app/databinding/databinding.component.ts
+++ src/app/databinding/databinding.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class DatabindingComponent implements OnInit {
 
   carNm: string;
+  ipTyp = 'button';
+
   constructor() {
     this.carNm = 'Mercedez';
   }
